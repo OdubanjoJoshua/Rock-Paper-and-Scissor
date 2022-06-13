b1ce28983ae35e34d10d73e7f8ec3893524caa8e
Joshua Odubanjo <joshuaodubanjo@yahoo.com>
2022-06-13 03:10:14 +0100

    Declaration of the game rules

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+# Welcome greeting and declaration of rules
+welcome = 'Rock Paper and Scissors game!!!'
+rule = ''' 
+--- This is the rule of the game ---
+If the two players choose the same character it's a tie, and the game repeats
+Rock beats Scissors
+Paper beats Rock
+Scissors beats Paper
+ '''
+print(welcome, rule)
