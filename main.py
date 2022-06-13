27bedf7c53b77fd7ddc44cfbcca36b69fee65c85
Joshua Odubanjo <joshuaodubanjo@yahoo.com>
2022-06-13 03:13:15 +0100

    Game option, creating user and computer input

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ Paper beats Rock
 Scissors beats Paper
  '''
 print(welcome, rule)
+
+# Game option in a list
+game_option = ['Rock', 'Paper', 'Scissors']
+
+# User and computer input
+player_1 = 0 #This is the user
+player_2 = 0 #This is the computer
